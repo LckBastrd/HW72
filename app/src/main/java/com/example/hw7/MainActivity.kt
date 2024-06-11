c5ee7085be7ac52e8f7bf23e67f729f631b09d4d
Viacheslav Silchenko <122287358+LckBastrd@users.noreply.github.com>
2024-06-11 20:58:23 +0300

    MainActivity.kt

--- app/src/main/java/com/example/hw7/MainActivity.kt
+++ app/src/main/java/com/example/hw7/MainActivity.kt
@@ -54,7 +54,7 @@ fun PhotoDetailsPage() {
                 painter = painterResource(id = R.drawable.bangkok4top),
                 contentDescription = null,
                 modifier = Modifier
-                    .fillMaxSize() // Растягиваем изображение на всю ширину
+                    .fillMaxSize()
             )
             Row(
                 modifier = Modifier
@@ -76,7 +76,7 @@ fun PhotoDetailsPage() {
             }
         }
 
-        Spacer(modifier = Modifier.height(16.dp)) // Увеличиваем отступ сверху
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
@@ -98,43 +98,43 @@ fun PhotoDetailsPage() {
                     painter = painterResource(id = R.drawable.download),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(36.dp) // Увеличиваем размер иконок
+                        .size(36.dp)
                         .padding(horizontal = 8.dp)
                 )
                 Image(
                     painter = painterResource(id = R.drawable.favorite),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(36.dp) // Увеличиваем размер иконок
+                        .size(36.dp)
                         .padding(horizontal = 8.dp)
                 )
                 Image(
                     painter = painterResource(id = R.drawable.bookmark),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(36.dp) // Увеличиваем размер иконок
+                        .size(36.dp)
                         .padding(horizontal = 8.dp)
                 )
             }
         }
 
-        Spacer(modifier = Modifier.height(16.dp)) // Увеличиваем отступ сверху
-        Divider(color = Color.White.copy(alpha = 0.5f)) // Добавляем разделительную линию
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider(color = Color.White.copy(alpha = 0.5f))
 
         CameraDetails()
 
-        Spacer(modifier = Modifier.height(16.dp)) // Увеличиваем отступ сверху и снизу
-        Divider(color = Color.White.copy(alpha = 0.5f)) // Добавляем разделительную линию
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider(color = Color.White.copy(alpha = 0.5f))
 
         StatsSection()
 
-        Spacer(modifier = Modifier.height(16.dp)) // Увеличиваем отступ сверху и снизу
-        Divider(color = Color.White.copy(alpha = 0.5f)) // Добавляем разделительную линию
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider(color = Color.White.copy(alpha = 0.5f))
 
-        Spacer(modifier = Modifier.height(16.dp)) // Увеличиваем дополнительное пространство перед кнопками
+        Spacer(modifier = Modifier.height(16.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start // Сдвигаем кнопки к левому краю
+            horizontalArrangement = Arrangement.Start
         ) {
             Chip("Barcelona")
             Spacer(modifier = Modifier.width(8.dp))
@@ -153,7 +153,7 @@ fun CameraDetails() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 8.dp), // Увеличиваем отступы сверху и снизу
+                .padding(vertical = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Column(
@@ -161,7 +161,7 @@ fun CameraDetails() {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Camera", color = Color.Gray, fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp)) // Увеличиваем отступы между строками
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = "NIKON D3200", color = Color.White, fontSize = 14.sp)
             }
             Column(
@@ -169,14 +169,14 @@ fun CameraDetails() {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Aperture", color = Color.Gray, fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp)) // Увеличиваем отступы между строками
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = "f/5.0", color = Color.White, fontSize = 14.sp)
             }
         }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 8.dp), // Увеличиваем отступы сверху и снизу
+                .padding(vertical = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Column(
@@ -184,7 +184,7 @@ fun CameraDetails() {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Focal Length", color = Color.Gray, fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp)) // Увеличиваем отступы между строками
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = "18.0mm", color = Color.White, fontSize = 14.sp)
             }
             Column(
@@ -192,14 +192,14 @@ fun CameraDetails() {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Shutter Speed", color = Color.Gray, fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp)) // Увеличиваем отступы между строками
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = "1/125s", color = Color.White, fontSize = 14.sp)
             }
         }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 8.dp), // Увеличиваем отступы сверху и снизу
+                .padding(vertical = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Column(
@@ -207,7 +207,7 @@ fun CameraDetails() {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "ISO", color = Color.Gray, fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp)) // Увеличиваем отступы между строками
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = "100", color = Color.White, fontSize = 14.sp)
             }
             Column(
@@ -215,7 +215,7 @@ fun CameraDetails() {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Dimensions", color = Color.Gray, fontSize = 14.sp)
-                Spacer(modifier = Modifier.height(8.dp)) // Увеличиваем отступы между строками
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = "3906 × 4882", color = Color.White, fontSize = 14.sp)
             }
         }
@@ -227,7 +227,7 @@ fun StatsSection() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 32.dp), // Добавляем отступы слева и справа
+            .padding(horizontal = 32.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         StatItem(label = "Views", value = "8.8M")
@@ -240,7 +240,7 @@ fun StatsSection() {
 fun StatItem(label: String, value: String) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(vertical = 8.dp) // Увеличиваем отступы сверху и снизу
+        modifier = Modifier.padding(vertical = 8.dp)
     ) {
         Text(text = value, color = Color.White, fontSize = 16.sp, fontWeight = FontWeight.Bold)
         Text(text = label, color = Color.Gray, fontSize = 14.sp)
